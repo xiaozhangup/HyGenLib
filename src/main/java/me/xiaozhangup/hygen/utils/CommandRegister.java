e873327b88bac72d15c86113e4d2313a11a8dd02
Daniel Zhang <xiaozhangshiw@163.com>
2023-03-20 23:42:06 +0800

    CommandRegister and EventRegister

--- src/main/java/me/xiaozhangup/hygen/utils/CommandRegister.java
+++ src/main/java/me/xiaozhangup/hygen/utils/CommandRegister.java
@@ -1,4 +1,42 @@
 package me.xiaozhangup.hygen.utils;
 
-public class CommandRegister {
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+
+public abstract class CommandRegister implements CommandExecutor {
+
+    private final String name;
+    private final PluginCommand command;
+
+    public CommandRegister(String name) {
+        this.name = name;
+
+        PluginCommand pluginCommand = Bukkit.getPluginCommand(name);
+        if (pluginCommand == null) {
+            throw new RuntimeException("Cannot find plugin command '" + name + "'.");
+        }
+
+        this.command = pluginCommand;
+        pluginCommand.setExecutor(this);
+    }
+
+    public static boolean register(String name, CommandExecutor executor) {
+        PluginCommand pluginCommand = Bukkit.getPluginCommand(name);
+        if (pluginCommand == null) {
+            return false;
+        }
+
+        pluginCommand.setExecutor(executor);
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PluginCommand getCommand() {
+        return command;
+    }
+
 }
--- src/main/java/me/xiaozhangup/hygen/utils/EventRegister.java
+++ src/main/java/me/xiaozhangup/hygen/utils/EventRegister.java
@@ -1,4 +1,48 @@
 package me.xiaozhangup.hygen.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public class EventRegister {
+
+    private final ArrayList<Listener> listeners = new ArrayList<>();
+    private final Plugin plugin;
+    private boolean log = false;
+
+    public EventRegister(Plugin plugin, boolean log) {
+        this.plugin = plugin;
+        this.log = log;
+    }
+
+    public EventRegister(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void addListener(Listener listener) {
+        listeners.add(listener);
+    }
+
+    public void addListeners(Listener... listener) {
+        listeners.addAll(Arrays.asList(listener));
+    }
+
+    public void addListeners(Collection<? extends Listener> listener) {
+        listeners.addAll(listener);
+    }
+
+    public void register() {
+        listeners.forEach(listener -> {
+            Bukkit.getPluginManager().registerEvents(listener, this.plugin);
+            if (this.log) {
+                this.plugin.getLogger().info("Registration " + listener.getClass().getName() + "...");
+            }
+        });
+        listeners.clear();
+    }
+
 }
